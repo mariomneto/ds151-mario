052692079d650e985b78cada8c6f566f7fac2b55
Mario Morais Neto <mariom_mmn@hotmail.com>
2023-04-16 21:14:03 -0300

    navigation

--- App.tsx
+++ App.tsx
@@ -1,38 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
 
-
+const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>Inicio</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreen: React.FC = () => {
+    return (
+        <View>
+            <Text>Inicio</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex"
+    }
+});
+
+export default HomeScreen;
